1cfd9dbb2cb65314228bdd47400daa0a6c9163b6
marcostorresf <marcos_e19@icloud.com>
2025-02-22 00:25:45 +0000

    Done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ['the', 'our', 'his', 'hers'];
+  let adj = ['great', 'big', 'little', 'small'];
+  let noun = ['jogger', 'racoon', 'democrat', 'republican'];
+  let domain = ['.com', '.net', '.org', '.gov'];
+  
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let b = 0; b < noun.length; b++) {
+        for (let c = 0; c < domain.length; c++) {
+        console.log(pronoun[i] + adj[a] + noun[b] + domain[c]);
+        }
+      }
+    }
+  }
+
   console.log("Hello Rigo from the console!");
 };
