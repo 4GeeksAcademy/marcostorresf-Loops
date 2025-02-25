80db89e2345a9becd0fe9f2fe33e0dbf91cc61ec
marcostorresf <marcos_e19@icloud.com>
2025-02-25 00:28:22 +0000

    Done

--- src/app.js
+++ src/app.js
@@ -6,16 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let pronoun = ['the', 'our', 'his', 'hers'];
-  let adj = ['great', 'big', 'little', 'small'];
-  let noun = ['jogger', 'racoon', 'democrat', 'republican'];
-  let domain = ['.com', '.net', '.org', '.gov'];
-  
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let a = 0; a < adj.length; a++) {
-      for (let b = 0; b < noun.length; b++) {
-        for (let c = 0; c < domain.length; c++) {
-        console.log(pronoun[i] + adj[a] + noun[b] + domain[c]);
+  let pronouns = ['the', 'our', 'his', 'hers'];
+  let adjs = ['great', 'big', 'little', 'small'];
+  let nouns = ['jogger', 'racoon', 'democrat', 'republican'];
+  let domains = ['.com', '.net', '.org', '.gov'];
+
+  // Java program to demonstrates the working "of" for loop
+  // for loop to generate domain names
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let domain of domains) {
+        console.log(pronoun+adj+noun+domain);
         }
       }
     }
